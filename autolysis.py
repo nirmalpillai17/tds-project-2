3ca4bd31222e69e5aa49d22e18a30be8914bfb63
Nirmal S <nirmalpillai24@gmail.com>
2024-12-09 01:05:53 +0530

    remove api token

--- autolysis.py
+++ autolysis.py
@@ -1,5 +1,5 @@
 import requests
 
-API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIzZjEwMDMwNDBAZHMuc3R1ZHkuaWl0bS5hYy5pbiJ9.d5ptSimplm5j7uCyVscfpmyKG2h5OLP4SS-9cgsHmuE"
+API_TOKEN = "tobefilledin"
 response = requests.get("https://to.be.filled.in")
 
